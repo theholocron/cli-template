1feb5490181aaefb8e3c96da768734f7b9e1d2e1
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-12 11:56:26 -0700

    refactor(deploy): rename deploy-docs/storybook to deploy in config and workflow
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- holocron.config.ts
+++ holocron.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
 		"audit",
 		{ name: "test", with: { "run-unit": true } },
 		{ name: "release", with: { "run-build": true } },
-		{ name: "deploy-docs", with: { name: "cli-template" }, paths: ["docs/**"] },
+		{ name: "deploy", with: { type: "docs", name: "cli-template" }, paths: ["docs/**"] },
 	],
 	providers: {
 		...providers,
